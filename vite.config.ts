6f135b7e208336c907544863aefe155f49a12e90
Harsh Kanaujiya <154357957+harshkanaujia28@users.noreply.github.com>
2026-05-20 14:03:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    TanStackStartVite(),
+    tanstackRouter(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
